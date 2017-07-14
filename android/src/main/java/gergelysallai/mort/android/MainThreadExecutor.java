5e06a492fbcb46a7addef8f045e8221da2f32b67
Gergely Sallai <gergely-sallai@users.noreply.github.com>
2017-07-14 23:43:05 +0200

    Android app basics

---
+++ android/src/main/java/gergelysallai/mort/android/MainThreadExecutor.java
@@ -0,0 +1,30 @@
+package gergelysallai.mort.android;
+
+import android.os.Handler;
+import android.os.Looper;
+import android.support.annotation.NonNull;
+
+import java.util.concurrent.Executor;
+
+class MainThreadExecutor implements Executor {
+
+    private final Handler handler;
+    private final Thread mainThread;
+
+    public MainThreadExecutor() {
+        final Looper looper = Looper.getMainLooper();
+        this.mainThread = looper.getThread();
+        this.handler = new Handler(looper);
+    }
+
+    @Override
+    public void execute(@NonNull Runnable command) {
+        if (mainThread == Thread.currentThread()) {
+            command.run();
+        } else {
+            handler.post(command);
+        }
+    }
+
+}
+
